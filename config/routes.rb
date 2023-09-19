0cef0eba7ea569c7f2f9b66570c967586b56953d
高橋亜友美 <kooszs12@gmail.com>
2023-09-19 11:11:15 +0000

    [Finish] devise/user

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  # ユーザー用
+  # URL /customers/sign_in ...（URLが自動で変更してくれる）
+  # Webページに表示されるものを制限かける
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "user/registrations",
+    sessions: 'public/sessions'
+  }
+
   # 管理者用
   # URL /admin/sign_in ...
-  # Webページに表示されるものを制限かける
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
