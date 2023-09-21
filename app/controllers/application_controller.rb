e368ef4849106f1066632577d952d2f9c658072e
高橋亜友美 <kooszs12@gmail.com>
2023-09-21 09:49:46 +0000

    [Add] user_sign_up/名前入力フォーム

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  
+  # サインアップ時にnameも許可
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
 
 # deviseログインを手動で別ページに遷移させるメソッド
