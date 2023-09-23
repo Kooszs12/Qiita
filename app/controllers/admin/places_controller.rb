1720a24d7d28772ed7f6c0968106da64bb391054
高橋亜友美 <kooszs12@gmail.com>
2023-09-23 11:22:15 +0000

    [Add] admin/寺社投稿機能

--- app/controllers/admin/places_controller.rb
+++ app/controllers/admin/places_controller.rb
@@ -4,9 +4,21 @@ class Admin::PlacesController < ApplicationController
   before_action :authenticate_admin!
 
   def new
+    @place = Place.new
   end
 
   def create
+    # 許可されたデータが変数に格納
+    @place = Place.new(place_params)
+    # 保存が成功した場合
+    if @place.save
+      # フラッシュメッセージを表示して寺社詳細ページへ遷移
+      redirect_to admin_places_path(@place), info: "投稿されました"
+    # それ以外の場合（失敗した場合）
+    else
+      # 寺社新規投稿ページへ遷移
+      render :new
+    end
   end
 
   def index
@@ -43,7 +55,12 @@ class Admin::PlacesController < ApplicationController
     :fee,
     :start_time,
     :end_time
-    )
+    ).merge(
+        # ユーザーIDが存在するかどうか判断。存在しなかった場合nil
+        user_id: user_signed_in? ? current_user.id : nil,
+        # アドミンIDが存在するかどうか判断。存在しなかった場合nil
+        admin_id: admin_signed_in? ? current_admin.id : nil
+      )
   end
 
 end
