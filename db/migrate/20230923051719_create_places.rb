cd5f24343a0c7382c30945e2b8f5d2cb13d95cf3
高橋亜友美 <kooszs12@gmail.com>
2023-09-23 05:30:35 +0000

    [Add] Model作成:Area/Prefecture/Place

---
+++ db/migrate/20230923051719_create_places.rb
@@ -0,0 +1,37 @@
+class CreatePlaces < ActiveRecord::Migration[6.1]
+  def change
+    create_table :places do |t|
+
+      # Prefectureとのアソシエーション
+      t.references :prefecture, null: false, foreign_key: true
+      # Userとのアソシエーション
+      t.integer :user_id #拡張性を考慮
+      # Adminとのアソシエーション
+      t.integer :admin_id #拡張性を考慮
+      # お寺or神社のカラム
+      t.integer :category, null: false, default: 0 # enum
+      # 寺社名カラム
+      t.string :name, null: false
+      # 市区町村からの住所カラム
+      t.string :address, null: false
+      # 郵便番号カラム
+      t.string :postcode, null: false
+      # 電話番号カラム
+      t.string :phone_number
+      # 神様カラム
+      t.string :got
+      # 宗派カラム
+      t.string :sect
+      # 拝観料カラム
+      t.integer :fee, default: 0 # 金額のカラムは, defaultを０にしておいた方がいい
+      # 営業開始カラム
+      t.time :start_time
+      # 営業終了
+      t.time :end_time
+      # ペットカラム
+      t.integer :pet, null: false, default: 0 #enum
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,32 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 #二回目rails db:seedするときは、元々登録してた記述をコメントアウトする
-Admin.create!(
-  email: 'admin51065@email.com',
-  password: '202306'
-)
+# Admin.create!(
+#   email: 'admin51065@email.com',
+#   password: '202306'
+# )
+
+#地方と都道府県の配列
+area_prefectures = [
+  ["北海道・東北地方", ["北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県"]],
+  ["関東地方", ["茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県"]],
+  ["中部地方", ["新潟県", "富山県", "石川県", "福井県",  "山梨県", "長野県", "岐阜県", "静岡県", "愛知県"]],
+  ["近畿地方", ["三重県", "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県"]],
+  ["中国・四国地方", ["鳥取県", "島根県", "岡山県", "広島県", "山口県","徳島県", "香川県", "愛媛県", "高知県"]],
+  ["九州・沖縄地方", ["福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"]],
+]
+
+#h配列からデータ取得、地方と都道府県のペア組んで保存
+area_prefectures.each do |area_prefecture|
+  # area_prefecture配列から地方名と都道府県名のペアを取得
+  area_name = area_prefecture[0]
+  # 地方レコードを作成し、データベースに保存する
+  area = Area.create!(name: area_name)
+  # 都道府県名の配列を取得
+  prefecture_names = area_prefecture[1]
+  # 各都道府県名を処理
+  prefecture_names.each do |prefecture_name|
+    # 都道府県レコードを作成し、対応する地方に紐付けてデータベースに保存する
+    area.prefectures.create!(name: prefecture_name)
+  end
+end
